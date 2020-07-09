be9f2c118e132ac0cfb7edf4fde293c05dbf8fc1
Mohamed Homaid <mohamed.homaid@yama-sol.com>
2020-07-09 00:28:20 -0400

    Added deploy-connector script which helps deploy the connectors to confluent connect

---
+++ bin/deploy-connector.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+CONN_HOME=/Users/mohamed.homaid/Apache/Pinot/kafka-connect-apache-pinot-source/target/kafka-connect-target/usr/share/kafka-connect
+CONFLUENT_CONN=/Users/mohamed.homaid/Confluent/platform/confluent-5.5.1/share/java/pinot
+cp -R $CONN_HOME/* $CONFLUENT_CONN
+confluent local stop connect
+sleep 10
+confluent local start connect
