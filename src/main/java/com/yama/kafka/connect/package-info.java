2690f899289d2fa04960a07234e45dd974d60bb3
Mohamed Homaid <mohamed.homaid@yama-sol.com>
2020-07-08 23:47:57 -0400

    initial commit

---
+++ src/main/java/com/yama/kafka/connect/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * This attribute is used during documentation generation to write the introduction section.
+ */
+@Introduction("This plugin is used to add additional JSON parsing functionality to Kafka Connect.")
+/**
+ * This attribute is used as the display name during documentation generation.
+ */
+@Title("kafka-connect-apache-pinot-source")
+/**
+ * This attribute is used to provide the owner on the connect hub. For example jcustenborder.
+ */
+@PluginOwner("com.yama.kafka.connect")
+/**
+ * This attribute is used to provide the name of the plugin on the connect hub.
+ */
+@PluginName("kafka-connect-apache-pinot-source")
+package com.yama.kafka.connect;
+
+import com.github.jcustenborder.kafka.connect.utils.config.Introduction;
+import com.github.jcustenborder.kafka.connect.utils.config.PluginName;
+import com.github.jcustenborder.kafka.connect.utils.config.PluginOwner;
+import com.github.jcustenborder.kafka.connect.utils.config.Title;
---
+++ src/test/java/com/yama/kafka/connect/DocumentationTest.java
@@ -0,0 +1,7 @@
+package com.yama.kafka.connect;
+
+import com.github.jcustenborder.kafka.connect.utils.BaseDocumentationTest;
+
+public class DocumentationTest extends BaseDocumentationTest {
+
+}
