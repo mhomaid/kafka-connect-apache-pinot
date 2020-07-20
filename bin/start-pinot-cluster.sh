a650d9c1269a1e0553b1af473a2e87552754caaf
Mohamed Homaid <mhomaid@gmail.com>
2020-07-20 19:50:17 -0400

    moved docker-compose.yml

--- bin/start-pinot-cluster.sh
+++ bin/start-pinot-cluster.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Set env 
-PINOT_VERSION=0.4.0
-PINOT_HOME=~/Apache/Pinot/apache-pinot-incubating-${PINOT_VERSION}-bin
+PINOT_VERSION=0.5.0
+PINOT_HOME=/Users/mohamed.homaid/Apache/Pinot/apache-pinot-incubating-0.5.0-SNAPSHOT-bin
+cd $PINOT_HOME
 
 #### Start Zookeeper on port 2191 ################
 ${PINOT_HOME}/bin/pinot-admin.sh StartZookeeper -zkPort 2191
@@ -9,7 +10,7 @@ sleep 15
 #### Start 2 controllers on ports 9001,9002 ################
 ${PINOT_HOME}/bin/pinot-admin.sh StartController -zkAddress localhost:2191 -controllerPort 9001
 sleep 5
-${PINOT_HOME}/bin/pinot-admin.sh StartController -zkAddress localhost:2191 -controllerPort 9002
+${PINOT_HOME}/bin/pinot-admin.sh StartController -zkAddress localhost:2191 -controllerPort 9000
 sleep 10
 #### Start 2 brokers on ports 7001,7002 ################
 ${PINOT_HOME}/bin/pinot-admin.sh StartBroker -zkAddress localhost:2191 -clusterName PinotCluster -brokerPort 7001
@@ -21,14 +22,27 @@ ${PINOT_HOME}/bin/pinot-admin.sh StartServer -zkAddress localhost:2191 -clusterN
 ${PINOT_HOME}/bin/pinot-admin.sh StartServer -zkAddress localhost:2191 -clusterName PinotCluster -serverPort 8002 -serverAdminPort 8012
 sleep 10
 ######
-BASE_DIR=~/Apache/Pinot/pinot-tutorial/transcript
+BASE_DIR=/Users/mohamed.homaid/Apache/Pinot/pinot-tutorial/transcript
+cd $BASE_DIR
 ${PINOT_HOME}/bin/pinot-admin.sh AddTable -tableConfigFile $BASE_DIR/transcript-table-offline.json -schemaFile $BASE_DIR/transcript-schema.json -controllerPort 9001 -exec
 
 #### Load the data #####
-${PINOT_HOME}/bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile ~/Apache/Pinot/pinot-tutorial/transcript/batch-job-spec.yml
+${PINOT_HOME}/bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile /Users/mohamed.homaid/Apache/Pinot/pinot-tutorial/transcript/batch-job-spec.yml
 
 ${PINOT_HOME}/bin/pinot-admin.sh AddTable \
-  -tableConfigFile ~/Apache/Pinot/pinot-quick-start/transcript-table-offline.json \
-  -schemaFile ~/Apache/Pinot/pinot-quick-start/transcript-schema.json -controllerPort 9001 -exec
+  -tableConfigFile /Users/mohamed.homaid/Apache/Pinot/pinot-quick-start/transcript-table-offline.json \
+  -schemaFile /Users/mohamed.homaid/Apache/Pinot/pinot-quick-start/transcript-schema.json -controllerPort 9001 -exec
 
-${PINOT_HOME}/bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile ~/Apache/Pinot/pinot-quick-start/batch-job-spec.yml
+${PINOT_HOME}/bin/pinot-admin.sh LaunchDataIngestionJob -jobSpecFile /Users/mohamed.homaid/Apache/Pinot/pinot-quick-start/batch-job-spec.yml
+
+bin/pinot-admin.sh  StartKafka -zkAddress=localhost:2191/kafka -port 9876
+bin/kafka-topics.sh --create --bootstrap-server localhost:9876 --replication-factor 1 --partitions 1 --topic transcript-topic
+
+bin/pinot-admin.sh AddTable \
+    -schemaFile /Users/mohamed.homaid/Apache/Pinot/pinot-quick-start/transcript-schema.json \
+    -tableConfigFile /Users/mohamed.homaid/Apache/Pinot/pinot-quick-start/transcript-table-realtime.json \
+    -controllerPort 9001 -exec
+
+kafka-console-producer \
+    --broker-list localhost:9876 \
+    --topic transcript-topic < /Users/mohamed.homaid/Apache/Pinot/pinot-quick-start/rawData/transcript.json
